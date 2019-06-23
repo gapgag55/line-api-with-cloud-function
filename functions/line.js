898c71925e30c7ff69ca26fd325a2d4a39695a01
Sarayut <imkopkap@gmail.com>
2019-06-24 01:03:55 +0700

    Add LineLogin API

--- functions/line.js
+++ functions/line.js
@@ -1,11 +1,17 @@
 const line = require('@line/bot-sdk');
 
-const config = {
+const lineLogin = {
+  channelId: '1591386978',
+  channelSecret: '530254a074e37f93a067c66ae1dea5f9'
+};
+
+const messaging = {
   channelAccessToken: 'mb2h9AU7u+VRJ7yDAsX9UhdpGEgb0JUMNrVGd7rJlMvwEVZsRRWGMd9KctMUUJO7e7/1yfURhU+3mSwyooyTPBf20gGDJZ2BICaZopgEZplxiqKB8G7ZKvIwSt3VYgukHHPo6FNhCtLZhVjszp/2FAdB04t89/1O/w1cDnyilFU=',
   channelSecret: '35132dad74990b001076a8c4a6f8cc67'
 };
 
 module.exports = {
-  client: new line.Client(config),
-  middleware: line.middleware(config)
+  client: new line.Client(messaging),
+  middleware: line.middleware(messaging),
+  lineLogin,
 };
--- functions/routes.js
+++ functions/routes.js
@@ -1,10 +1,12 @@
-const { client, middleware } = require('./line');
+const fetch = require('node-fetch');
+const qs = require('qs');
+const { lineLogin, client, middleware } = require('./line');
 const { textMessage, imageMessage, videoMessage } = require('./message');
 const mock = require('./mock.js');
 
 function webhook(req, res) {
   const { events } = req.body;
-
+  console.log(events);
   Promise
     .all(events.map(event => eventHandle(event)))
     .then(result => res.status(200).send('success'))
@@ -37,7 +39,60 @@ function videoHandle(event) {
   client.pushMessage(event.source.userId, videoMessage(mock.video.originalContentUrl, mock.video.previewImageUrl));
 }
 
+function auth(req, res) {
+  // console.log(req.body);
+
+  // Step1: User goes to "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1591386978&redirect_uri=https://us-central1-lineoa-celeb.cloudfunctions.net/nipa/auth&state=12345abcde&scope=openid%20profile&nonce=09876xyz"
+
+  // Step2: Get code from Step 1 via redirect URL [GET]
+  const code = req.query.code;
+
+  // Step3: Redirect URL sends code to get accessToken [POST] https://api.line.me/oauth2/v2.1/token grant_type, client_id, client_secret, code, redirect_uri
+  const data = {
+    grant_type: 'authorization_code',
+    client_id: lineLogin.channelId,
+    client_secret: lineLogin.channelSecret,
+    code,
+    redirect_uri: 'https://us-central1-lineoa-celeb.cloudfunctions.net/nipa/auth'
+  };
+
+  fetch('https://api.line.me/oauth2/v2.1/token', {
+    method: 'post',
+    body: qs.stringify(data),
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+  })
+  .then(res => res.json())
+  .then(response => {
+    const { access_token } = response;
+    req.body.accessToken = access_token;
+    profile(req, res);
+    return true;
+  })
+  .catch(error => res.send(error));
+}
+
+function profile(req, res) {
+  const { accessToken  } = req.body;
+
+  // Step 4: Get user profile GET https://api.line.me/v2/profile Authorization: Bearer {access token}
+  fetch('https://api.line.me/v2/profile', {
+    headers: { 'Authorization': `Bearer ${accessToken}` },
+  })
+  .then(res => res.json())
+  .then(response => {
+    const { userId } = response;
+    res.send(userId);
+    return true;
+  })
+  .catch(error => res.json(error));
+
+  // Step 5: Done!
+}
+
+
 module.exports = function(app) {
   app.post('/webhook', webhook);
+  app.post('/profile', profile);
+  app.get('/auth', auth);
   // app.post('/webhook', middleware, webhook);
 }
